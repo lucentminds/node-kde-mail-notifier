6fcfa22688597a54da55cdba587066c42aacf482
lucentminds <scott@lifecorp.com>
2017-09-01 16:25:26 -0500

    Updated to use kdialog instead of notify-send.

--- notify.sh
+++ notify.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-DISPLAY=:0.0 XAUTHORITY=~/.Xauthority notify-send "$1" -t 86400000
+#DISPLAY=:0.0 XAUTHORITY=~/.Xauthority notify-send "$1" -t 86400000
+DISPLAY=:0.0 XAUTHORITY=~/.Xauthority kdialog \
+   --passivepopup "$1" 86400000 \
+   --title "New Mail" \
+   --icon ""
