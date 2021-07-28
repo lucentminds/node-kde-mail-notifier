b021f39c15916e93cd55ae9802d7c1ab522a0e2e
lucentminds <scott@lifecorp.com>
2021-07-28 13:24:54 -0500

    Updated for kubuntu.

--- repo/kde-notify/notify.sh
+++ repo/kde-notify/notify.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 #DISPLAY=:0.0 XAUTHORITY=~/.Xauthority notify-send "$1" -t 86400000
-DISPLAY=:0.0 XAUTHORITY=~/.Xauthority kdialog \
-   --passivepopup "$1" 86400000 \
-   --title "New Mail" \
-   --icon ""
+# DISPLAY=:0.0 XAUTHORITY=~/.Xauthority kdialog \
+#    --passivepopup "$1" 86400000 \
+#    --title "New Mail" \
+#    --icon ""
+kdialog --title "New Mail" \
+   --passivepopup "$1" 86400000
